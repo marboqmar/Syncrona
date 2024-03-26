6e2103597350a5715b679c6cb4d1b614b8f46ee1
marboqmar <martamartinezboquer@gmail.com>
2024-03-26 20:18:48 +0100

    Added header and footer components

---
+++ src/scss/wrappers/MainWrapper.tsx
@@ -0,0 +1,12 @@
+import '../components/Header.tsx'
+import '../components/Footer.tsx'
+
+const MainWrapper = () => {
+    return (
+        <div>
+            <h1>Main wrapper</h1>
+        </div>
+    );
+};
+
+export default MainWrapper;
