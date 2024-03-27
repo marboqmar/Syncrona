b6ebc07d54e1a60998459f58005577e9cc3f966f
migromcab <migromcab@icloud.com>
2024-03-27 16:19:02 +0100

    adding font family

--- src/scss/views/Patterns.tsx
+++ src/scss/views/Patterns.tsx
@@ -1,13 +1,21 @@
 import MainWrapper from "../wrappers/MainWrapper.tsx";
+import "../style.scss"
+
+
+
 
 const Patterns = () => {
     return (
         <MainWrapper>
-            <div>
-
+            <div className={styles.container}>
+                <h1 className={`text ${styles.title}`}>Título</h1> 
+                <p className={`text ${styles.paragraph}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> 
+                <button className={`text ${styles.button}`}>Botón</button> 
             </div>
         </MainWrapper>
     );
 };
 
 export default Patterns;
+
+
