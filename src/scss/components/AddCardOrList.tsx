c5fb8112cea8b25e463d3e462ae95fe880a9a733
José <148952612+jocofe@users.noreply.github.com>
2024-04-16 20:31:23 +0200

    changing css aspects

--- src/scss/components/AddCardOrList.tsx
+++ src/scss/components/AddCardOrList.tsx
@@ -2,36 +2,36 @@ import React, { ReactElement, useState } from "react";
 import { Card, Paper } from "@material-ui/core";
 import AddCardOrListText from "./AddCardOrListText";
 import { ErrorTwoTone } from "@material-ui/icons";
+import EditIcon from "@material-ui/icons/Edit";
 import { Cards } from "./models";
 
 const AddCardOrList = () => {
-  const [card,setCard] = useState<Cards[]> ([]);
-  const createNewCard = ( ) => {
-  const id = card.length + 1;
-  const newCard: Cards = {text:'', id};
-  setCard([...card,newCard]);
-
-
+  const [card, setCard] = useState<Cards[]>([]);
+  const createNewCard = () => {
+    const id = card.length + 1;
+    const newCard: Cards = { text: "", id };
+    setCard([...card, newCard]);
   };
   return (
     <>
-     
-
-      <Paper style={{background: "transparent"}}>
-      {card.map((Cards) => (
-        <Paper key={Cards.id} className="List__NewCard" >
-          <AddCardOrListText cardInfo={card}/>
-        </Paper>
-      ))}
-
+      <Paper style={{ background: "transparent" }}>
+        {card.map((Cards) => (
+          <div className="display__flex__between align__items__center">
+            <Paper key={Cards.id} className="List__NewCard">
+              <AddCardOrListText cardInfo={card} />
+            </Paper>
+            <EditIcon />
+          </div>
+        ))}
       </Paper>
-      
+
       <Paper
         className="List__AddCardOrListButton"
         style={{ display: "inline-block" }}
       >
-        
-        <button style={{ width: "100%" }} onClick={createNewCard}>Add new Card</button>
+        <button style={{ width: "100%" }} onClick={createNewCard}>
+          Add new Card
+        </button>
       </Paper>
     </>
   );
--- src/scss/components/BoardCard.tsx
+++ src/scss/components/BoardCard.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Paper } from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
 const BoardCard = () => {
-    return (
-       <Paper className="List__Card">
-              <input type="text" placeholder="Type something" className="input" />
-       </Paper>
-        
-    )
-}
+  return (
+    <div className="display__flex__between align__items__center">
+      <Paper className="List__Card">
+        <input type="text" placeholder="Type something" className="input" />
+      </Paper>
+      <EditIcon />
+    </div>
+  );
+};
 
-export default BoardCard
+export default BoardCard;
