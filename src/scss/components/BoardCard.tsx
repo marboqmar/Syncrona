8d3660a57dff67904c6381331a73db48c5fc176b
José <148952612+jocofe@users.noreply.github.com>
2024-04-22 18:28:20 +0200

    touching css

--- src/scss/components/BoardCard.tsx
+++ src/scss/components/BoardCard.tsx
@@ -3,8 +3,13 @@ import React from "react";
 const BoardCard = () => {
   return (
     <div className="display__flex__between align__items__center">
-      <div className="List__Card">
-        <input type="text" placeholder="Type something" className="input" />
+      <div className="List__Card shadow__effect">
+        <input
+          type="text"
+          placeholder="Type something"
+          className="input width__border__box"
+          style={{ width: "100%" }}
+        />
       </div>
       ¡
     </div>
--- src/scss/components/ListTitle.tsx
+++ src/scss/components/ListTitle.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const ListTitle = () => {
   return (
-    <div className="List__Title">
-      <input type="text" placeholder="Tittle" className="List__Tittle--Text" />
+    <div className="List__Title ">
+      <input
+        type="text"
+        placeholder="Tittle"
+        className="List__Tittle--Text shadow__effect"
+      />
     </div>
   );
 };
