575471d58b7fe735b70a3c1b896c36a789049bea
migromcab <migromcab@icloud.com>
2024-04-29 16:07:51 +0200

    toggle element and darkmode useState

--- src/scss/components/topbar.tsx
+++ src/scss/components/topbar.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { ReactElement, useState } from "react";
 import styles from "../style.scss";
 import colors from "../colors.scss";
 import typography from "../typography.scss";
 
+
 const Topbar = () => {
+    const [dark,setDark] = useState(false);
+    const darkmode = () => {
+      setDark(!dark);
+    }
+
+
   return (
     <div className="topbar">
       <input type="text" placeholder="Tittle Page" />
-      <div className="switch">
-        <input type="checkbox" />
-        <span class="slider round"></span>
-      </div>
+      <label className="switch">
+      <input type="checkbox" onClick={darkmode}/>
+      <span className="slider round"></span>
+      </label>
     </div>
   );
 };
