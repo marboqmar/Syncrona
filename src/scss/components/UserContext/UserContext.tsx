836a05b153f08c67384e63e7b987b32a140fa086
migromcab <migromcab@icloud.com>
2024-05-21 14:57:15 +0200

    creating useState for userContextProvider

--- src/scss/components/UserContext/UserContext.tsx
+++ src/scss/components/UserContext/UserContext.tsx
@@ -55,10 +55,62 @@ interface UserContextProviderProps {
 export const UserContextProvider = ({
   children,
 }: UserContextProviderProps): JSX.Element => {
-  const contextValue = {};
+  const [tasks,setTasks] = useState<TaskModel[]>([]);
+  const [title,setTitle] = useState<TaskBoardModel[]>([]);
+  const [board,setBoard] = useState<TaskBoardModel[]>([]);
+  const newTaskBoard = (newBoard:TaskBoardModel) => {
+    setBoard(prevBoard => {
+      return [...prevBoard,newBoard]
+    })
+  };
+  const updateTaskBoardTitle = (newTitle: TaskModel) => {
+    setTitle(prevTitle => {
+      return [...prevTitle,newTitle]
+    })
+  };
+  const updateTask = (newTask: TaskModel) => {
+    setTasks(prevTaskState => {
+      return [...prevTaskState,newTask]
+    })
+  };
+
+
+
+
+  const contextValue = {
+    updateTask,
+    updateTaskBoardTitle,
+    newTaskBoard,
+
+  };
   return (
     <UserContextProvider value={{ contextValue }}>
       {children}
     </UserContextProvider>
   );
 };
+
+
+
+
+/*
+
+export const UserContextProvider = ({
+  children,
+}: UserContextProviderProps): JSX.Element => {
+  const [tasks, setTasks] = useState<TaskModel[]>([]);
+  const addTask = (newTask: TaskModel) => {
+    setTasks(prevTasksState => {
+       return [...prevTasksState, newTask];
+    });
+  }
+  const contextValue = {
+    addTask
+  };
+  return (
+    <UserContextProvider value={{ contextValue }}>
+      {children}
+    </UserContextProvider>
+  );
+};
+*/
