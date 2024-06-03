85e3c195716631cef724f5725cd1c0d5ad88b99f
José <148952612+jocofe@users.noreply.github.com>
2024-06-03 20:45:03 +0200

    styling inputs

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -9,41 +9,48 @@ const Login = () => {
     console.log("me clickearon banda");
   };
   return (
-    <div className="margin__center">
-      <Input
-        placeholder="username"
-        value={userName}
-        width="440px"
-        height="68px"
-        inputLabel="Username"
-        name="username"
-        setValue={setUserName}
-      />
-      <Input
-        placeholder="password"
-        value={password}
-        width="440px"
-        height="68px"
-        inputLabel="Password"
-        name="password"
-        setValue={setPassword}
-        type="password"
-      />
-      <Button
-        text="Log in"
-        type="primary"
-        onClick={logIn}
-        width={"155px"}
-        height={"55px"}
-      />
-      <p>tu user name: {userName}</p>
-      <p>tu contraseña: {password}</p>
-      <Checkbox
-        setChecked={setChecked}
-        text="henlo"
-        checked={checked}
-        name="cualquier nombre"
-      />
+    <div className="margin__center ">
+      <div className="bg__login ">
+        <div>
+          <Input
+            placeholder="username"
+            value={userName}
+            width="90%"
+            height="68px"
+            inputLabel="Username"
+            name="username"
+            setValue={setUserName}
+          />
+        </div>
+        <div style={{ borderRadius: "10px" }}>
+          <Input
+            placeholder="password"
+            value={password}
+            width="90%"
+            height="68px"
+            inputLabel="Password"
+            name="password"
+            setValue={setPassword}
+            type="password"
+          />
+        </div>
+
+        <Button
+          text="Log in"
+          type="primary"
+          onClick={logIn}
+          width={"155px"}
+          height={"55px"}
+        />
+        <p>tu user name: {userName}</p>
+        <p>tu contraseña: {password}</p>
+        <Checkbox
+          setChecked={setChecked}
+          text="henlo"
+          checked={checked}
+          name="cualquier nombre"
+        />
+      </div>
     </div>
   );
 };
