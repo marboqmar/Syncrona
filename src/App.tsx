797215ccf05218175a9e4f49fc6f31f034214977
sumikyu <lucia.angulopicazo@gmail.com>
2024-06-03 11:23:23 +0200

    modified board menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import './scss/style.scss'
-import { Link } from 'react-router-dom'
-import { Button } from './components/atoms'
-import { useState } from 'react';
+import "./scss/style.scss";
+import { Link } from "react-router-dom";
+import { Button } from "./components/atoms";
+import { useState } from "react";
 
-
-
-const funcionDelClick= ()=>{
-    console.log("me clickearon banda");
+const funcionDelClick = () => {
+  console.log("me clickearon banda");
 };
 
-
 function App() {
-    return (
-        <>
-            <h1>Home</h1>
-            <Link to='/login'>Login</Link>
-            <h1>Patterns</h1>
-            <Link to='/Patterns'>Patterns</Link>
-            <Button text='soy un botón' type='info' onClick={funcionDelClick} width={"180px"} height={"50px"}/>
-
-            <input/>
-
-        </>
-  )
+  return (
+    <>
+      <h1>Home</h1>
+      <Link to="/login">Login</Link>
+      <h1>Patterns</h1>
+      <Link to="/Patterns">Patterns</Link>
+      <Button
+        text="soy un botón"
+        type="info"
+        onClick={funcionDelClick}
+        width={"180px"}
+        height={"50px"}
+      />
+      <input />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/board-menu/board-menu.tsx
+++ src/board-menu/board-menu.tsx
@@ -6,14 +6,36 @@ const BoardListMenu = () => {
   return (
     <div>
       <div>
-        <Button>Atrás</Button>
+        <Button>
+          <img
+            src="../src/assets/icons/navigation/arrows/left/left arrow-2.svg"
+            alt="User"
+          />
+        </Button>
         <h1>Boards</h1>
         <Button>
-          <img src="../assets/settings/user.svg" alt="" />
+          <img src="../src/assets/icons/settings/user.svg" alt="User" />
+        </Button>
+        <Button>
+          <img
+            src="../src/assets/icons/settings/equalizer.svg"
+            alt="Settings"
+          />
         </Button>
       </div>
-      <div></div>
-      <div></div>
+      <div>
+        <li>
+          <a>
+            <img src="" alt="" />
+            <span>Mis cosas</span>
+          </a>
+        </li>
+      </div>
+      <div>
+        <Button>New board</Button>
+      </div>
     </div>
   );
 };
+
+export default BoardListMenu;
