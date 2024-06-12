07c73fcffb382e92d50303c5abb4bd26b427e8f4
migromcab <migromcab@icloud.com>
2024-06-12 15:51:37 +0200

    installing prettier

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -1,46 +1,34 @@
-import { useState } from "react";
-import {
-  Button,
-  Input,
-  Checkbox,
-  Radio,
-  Image,
-  Icon,
-  Logo,
-} from "../components/atoms";
-import Task from "../components/molecules/task";
-import "../scss/login.scss";
-import "../scss/style.scss";
-import { useTranslation } from "react-i18next";
-import Boards from "../boards/Boards";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Button, Input, Checkbox, Radio, Image, Icon, Logo } from '../components/atoms';
+import Task from '../components/molecules/task';
+import '../scss/login.scss';
+import '../scss/style.scss';
+import { useTranslation } from 'react-i18next';
+import Boards from '../boards/Boards';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  const { t, i18n } = useTranslation(["common", "list"]);
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const { t, i18n } = useTranslation(['common', 'list']);
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
   const [checked, setChecked] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(
-    "este radiobox en concreto"
-  );
+  const [selectedOption, setSelectedOption] = useState('este radiobox en concreto');
   const [task0, setTask0] = useState({
-    title: "",
+    title: '',
     positionId: 0,
     isFinished: false,
   });
   const logIn = () => {
-    console.log("me clickearon banda");
+    console.log('me clickearon banda');
   };
   return (
     <div>
       <div className="margin__center bg__login align__center ">
         <div>
           <div>
-            <p className="login__input__tittle  livvic-regular">
-              {t("common:Username")}
-            </p>
+            <p className="login__input__tittle  livvic-regular">{t('common:Username')}</p>
             <Input
-              placeholder={t("common:Username")}
+              placeholder={t('common:Username')}
               value={userName}
               width="80%"
               height="2rem"
@@ -51,9 +39,9 @@ const Login = () => {
             />
           </div>
           <div>
-            <p className="login__input__tittle">{t("common:Password")}</p>
+            <p className="login__input__tittle">{t('common:Password')}</p>
             <Input
-              placeholder={t("common:Password")}
+              placeholder={t('common:Password')}
               value={password}
               width="80%"
               height="2rem"
@@ -71,8 +59,8 @@ const Login = () => {
               text="Log in"
               type="primary"
               onClick={logIn}
-              width={"9.68rem"}
-              height={"3.43rem"}
+              width={'9.68rem'}
+              height={'3.43rem'}
               className="login__button  livvic-regular "
             />
           </Link>
@@ -80,31 +68,15 @@ const Login = () => {
       </div>
       <div className="justify__center">
         <div className="margin__center  TermsAndConditions flex__row ">
-          <Checkbox
-            setChecked={setChecked}
-            text="Terms and conditions"
-            checked={checked}
-            name="cualquier nombre"
-          />
-          <Checkbox
-            setChecked={setChecked}
-            text="Privacy policy"
-            checked={checked}
-            name="cualquier nombre"
-          />
+          <Checkbox setChecked={setChecked} text="Terms and conditions" checked={checked} name="cualquier nombre" />
+          <Checkbox setChecked={setChecked} text="Privacy policy" checked={checked} name="cualquier nombre" />
         </div>
         <div className="margin__center TermsAndConditions ">
           <div>
-            <button
-              onClick={() => i18n.changeLanguage("en")}
-              className="login__button--translation  livvic-regular "
-            >
+            <button onClick={() => i18n.changeLanguage('en')} className="login__button--translation  livvic-regular ">
               English
             </button>
-            <button
-              onClick={() => i18n.changeLanguage("es")}
-              className="login__button--translation  livvic-regular "
-            >
+            <button onClick={() => i18n.changeLanguage('es')} className="login__button--translation  livvic-regular ">
               Spanish
             </button>
           </div>
