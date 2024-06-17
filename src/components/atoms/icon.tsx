1b2d3cc5c350a1808d9f9230e838963b6fcb80a4
cocoraptor <149422082+cocoraptor@users.noreply.github.com>
2024-06-17 21:08:29 +0200

    feature:added the organism task board

--- src/components/atoms/icon.tsx
+++ src/components/atoms/icon.tsx
@@ -1,21 +1,21 @@
 import {FC, SVGProps} from "react";
 import icons from "../../assets/icons";
 import { IconProps,IconTypes,IconNames,SVGIconsInterface } from "./interfaces/icon.interface";
-//Esta es una función de envoltorio, pa poder meterle los íconos y no tener que hacer un componente 
+//Esta es una función de envoltorio, pa poder meterle los íconos y no tener que hacer un componente
 //por cada svg
-const withProps = (iconType: IconTypes,iconName: IconNames ) => ({theme,...props}: Partial<IconProps>) => {
+const withProps = (iconType: IconTypes,iconName: IconNames ) => ({theme, ...props}: Partial<IconProps>) => {
     //esto esta buscando los icons en el index por tipo, y de ahi el nombre específico del ícono
     const SvgIcons= icons[iconType] as SVGIconsInterface;
     const SvgIcon= SvgIcons[iconName] as unknown as FC<SVGProps<SVGSVGElement>>;
     return <SvgIcon {...props}/>;
 }
-const Icon = ({width, height, theme, iconName, iconType, className, onClick }:IconProps) => {
+const Icon = ({ width, height, theme, iconName, iconType, className, onClick }:IconProps) => {
     const IconWithProps = withProps(iconType,iconName)
-    const defaultOnClick =() =>{};
+    const defaultOnClick = () => {};
     return <IconWithProps
-        className={`${iconType} ${iconName} theme-${theme} ${className||''}`} 
-        width={width}
-        height={height}
+        className={`${iconType} ${iconName} theme-${theme} ${className||''}`}
+        width={width || '24px'}
+        height={height || '24px'}
         theme={theme}
         onClick={onClick || defaultOnClick}
         
