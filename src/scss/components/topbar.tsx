3954b8cb4e681906d0fe126e0e1abe380fa410bb
migromcab <migromcab@icloud.com>
2024-06-23 17:55:47 +0200

    adjusting darkmode

--- src/scss/components/topbar.tsx
+++ src/scss/components/topbar.tsx
@@ -1,54 +1,40 @@
-import  {  useState } from "react";
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useTranslation } from "react-i18next";
-import Icon from "../../components/atoms/icon";
+import { useTranslation } from 'react-i18next';
+import Icon from '../../components/atoms/icon';
+import '../style.scss';
 
 const Topbar = () => {
   const [dark, setDark] = useState(true);
   const darkmode = () => {
     if (dark) {
-      document.documentElement.style.setProperty("--color-text", "#fef5f9");
-      document.documentElement.style.setProperty("--main-color", "#211C1E");
+      document.documentElement.style.setProperty('--color-text', '#fef5f9');
+      document.documentElement.style.setProperty('--main-color', '#b5b0b2');
+      document.documentElement.style.setProperty('Syncrona-yellow', '#b5b0b2');
     } else {
-      document.documentElement.style.setProperty("--color-text", "#211C1E");
-      document.documentElement.style.setProperty("--main-color", "#fef5f9");
+      document.documentElement.style.setProperty('--color-text', '#211C1E');
+      document.documentElement.style.setProperty('--main-color', '#f9c06c');
     }
     setDark(!dark);
   };
 
-  const { t, i18n } = useTranslation(["common", "list"]);
+  const { t, i18n } = useTranslation(['common', 'list']);
 
   return (
     <div className="topbar">
       <div>
-      <Link to="/BoardsMenu">
-      <Icon className="Topbar__Icon"
-        iconName="LeftArrow2"
-        iconType="navigation"
-        height="20px"
-        fill="black"
-      />
-      </Link>
-      
+        <Link to="/BoardsMenu">
+          <Icon className="Topbar__Icon" iconName="LeftArrow2" iconType="navigation" height="20px" fill="black" />
+        </Link>
       </div>
 
-      <input
-        type="text"
-        placeholder={t("common:Tittle")}
-        className="Board__Page__Tittle livvic-regular"
-      />
-      <div >
+      <input type="text" placeholder={t('common:Tittle')} className="Board__Page__Tittle livvic-regular" />
+      <div>
         <div className="flex__row ">
-          <button
-            onClick={() => i18n.changeLanguage("en")}
-            className="move__Y Topbar__Options"
-          >
+          <button onClick={() => i18n.changeLanguage('en')} className="move__Y Topbar__Options">
             English
           </button>
-          <button
-            onClick={() => i18n.changeLanguage("es")}
-            className="move__Y Topbar__Options"
-          >
+          <button onClick={() => i18n.changeLanguage('es')} className="move__Y Topbar__Options">
             Spanish
           </button>
           <label className="switch">
