3e2bf8c5cabea0254fe951adabea4916deb590ae
cocoraptor <149422082+cocoraptor@users.noreply.github.com>
2024-06-26 06:21:41 +0200

    feature:added footer

--- src/scss/components/Footer.tsx
+++ src/scss/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Logo } from "../../components/atoms";
+import { Button, Icon, Input, Logo } from "../../components/atoms";
 
 const Footer = () => {
   return (
@@ -9,6 +9,11 @@ const Footer = () => {
           <div>
           <Input value="" type="email" placeholder="Enter e-mail"></Input>
           <Button className="syncrona-yellow" type='primary' height="60px" text='Register'></Button>
+          </div>
+          <div className="footer__icon">
+            <Icon iconType="social" iconName="Twitter" fill="white"></Icon>
+            <Icon iconType="social" iconName="Instagram"></Icon>
+            <Icon iconType="social" iconName="LinkedIn"></Icon>
           </div>
         </div>
         <div>
