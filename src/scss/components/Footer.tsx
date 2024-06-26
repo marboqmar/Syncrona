85bfa88e471bb44344abfe1a58fdbbef02bcae20
cocoraptor <149422082+cocoraptor@users.noreply.github.com>
2024-06-26 06:07:27 +0200

    feature:added Footer

--- src/scss/components/Footer.tsx
+++ src/scss/components/Footer.tsx
@@ -1,5 +1,25 @@
+import { Button, Input, Logo } from "../../components/atoms";
+
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <div className='bg__gradient'>
+      <div className="footer__flexbox_wrapper">
+        <Logo logoTheme="Pastel" width="80px"></Logo>
+        <div className="landing__wrapper_centered">
+          <div>
+          <Input value="" type="email" placeholder="Enter e-mail"></Input>
+          <Button className="syncrona-yellow" type='primary' height="60px" text='Register'></Button>
+          </div>
+        </div>
+        <div>
+          <></>
+        </div>
+      </div>
+      
+    </div>
+  
+
+  );
 };
 
 export default Footer;
